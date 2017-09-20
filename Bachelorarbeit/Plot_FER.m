672a1244c9cd0fbd5c0b2bf10c8b8c4c1517e719
Kevin Li <kev.li1010@gmail.com>
2017-09-20 17:36:56 +0200

    FER for 16 and 64 QAM

--- Bachelorarbeit/Plot_FER.m
+++ Bachelorarbeit/Plot_FER.m
@@ -1,15 +1,15 @@
-openfig('QPSK.fig');
+openfig('Cap_64QAM.fig');
 hold on;
-title('FER vs. Capacity of QPSK');
-load('snr_FER_1_2.mat');
-plot(snr_FER,1,'r*');
-load('snr_FER_2_3.mat');
-plot(snr_FER,(2*2/3),'r*');
-load('snr_FER_3_4.mat');
-plot(snr_FER,1.5,'r*');
-load('snr_FER_5_6.mat');
-plot(snr_FER,(2*5/6),'r*');
+title('FER vs. Capacity of 64-QAM');
+load('snr_FER_1_2_QAM64.mat');
+plot(snr_FER,3,'r*');
+load('snr_FER_2_3_A_QAM64.mat');
+plot(snr_FER,(6*2/3),'r*');
+load('snr_FER_3_4_A_QAM64.mat');
+plot(snr_FER,4.5,'r*');
+load('snr_FER_5_6_QAM64.mat');
+plot(snr_FER,(6*5/6),'r*');
 
-legend('QPSK','R = 1/2', 'R = 2/3', 'R = 3/4', 'R = 5/6');
-xlim([-10,20]);
-ylim([0, 2.5]);
+legend('64-QAM','R = 1/2', 'R = 2/3', 'R = 3/4', 'R = 5/6');
+xlim([-5,30]);
+ylim([0, 6.5]);
--- Bachelorarbeit/sim_QPSK.m
+++ Bachelorarbeit/sim_QPSK.m
@@ -16,32 +16,49 @@ snr_max = input(prompt3);
 h_Noise = log2(pi*exp(1));                                                              
 snr = snr_min:1:snr_max;
 snr_power = 10.^(snr/10);
-cap_QPSK = [];
+%% Non standard PSK distribution
+% 
 
+% input x = [x1, x2, x3,..., xN]
 
-for power = 1:length(snr)
-Amplitude_symbols = sqrt(snr_power(power));                                % Scaling of Amlitude through SNR  
-bits = round(4*rand(N,1));                                                 % random bits created for phaseshift (between 0-4 int)
-QPSK_symbols = Amplitude_symbols * exp(1i*(2*pi/4)*bits);                  % points shift by 90° to create QPSK
-Noise = (1/sqrt(2))*(randn(N,1)+1i*randn(N,1));                            % add complex noise
-Y   = QPSK_symbols + Noise;                                                  % Y = X + N
-Y_t = transpose(Y);
-X = Amplitude_symbols * exp(1i*(2*pi/4)*(1:4));                            % create (0,1),(0,-1),(-1,0),(1,0)
-X_t = transpose(X);
-
-%abs(y-xi)^2
-x = repmat(X_t,1,N);                                                       % duplicate X symbols into a 4xN matrix
-y = repmat(Y_t,4,1);                                                       % duplicate received symbols into a 4xN matrix
-s = -(abs(y - x)).^2;                                                      % calculate difference for probability calculation
-
-%h_signal
-prob_Y = (1/(4*pi))*sum(exp(s),1);                                         % probability calculation
-h_Y = (sum(-log2(prob_Y)))/N;                                              % entropy of received signal
-cap_QPSK(power) = (h_Y - h_Noise);                                         % capacity
+x_points = [-7-7j -7-5j -7-3j -7-1j -7+1j -7+3j -7+5j -7+7j -5-7j -5-5j -5-3j -5-1j -5+1j -5+3j -5+5j -5+7j -3-7j -3-5j -3-3j -3-1j -3+1j -3+3j -3+5j -3+7j -1-7j -1-5j -1-3j -1-1j -1+1j -1+3j -1+5j -1+7j 1-7j 1-5j 1-3j 1-1j 1+1j 1+3j 1+5j 1+7j 3-7j 3-5j 3-3j 3-1j 3+1j 3+3j 3+5j 3+7j 5-7j 5-5j 5-3j 5-1j 5+1j 5+3j 5+5j 5+7j 7-7j 7-5j 7-3j 7-1j 7+1j 7+3j 7+5j 7+7j];
+n = length(x_points);
+h_N = log2(pi*exp(1));
+scale = sum(abs(x_points).^2)/length(x_points);
+x_points = x_points./sqrt(scale);
+for l = 1:length(snr_power)
+    level = round((n-1)*rand(1,N)+1);
+    level_matrix = zeros(N,n);
+for k = 1:N
+    level_matrix(k,level(k)) = 1;
 end
-plot (snr,cap_QPSK);
+
+x_points_t = transpose(x_points);
+Amplitude = sqrt(snr_power(l));
+x_out = Amplitude*level_matrix*x_points_t;
+
+Noise = (1/sqrt(2))*(randn(N,1)+1i*randn(N,1));
+
+Y = x_out + Noise;
+Y_t = transpose(Y);
+X = Amplitude * x_points_t;
+
+y = repmat(Y_t,n,1);
+x = repmat(X,1,N);
+
+s = -((abs(y - x)).^2);
+    
+prob_Y = (1/(pi*n))*(sum(exp(s),1));
+    
+h_Y = sum(-log2(prob_Y))/N;
+
+cap_PSK(l) = h_Y - h_N;
+end
+figure;
+plot(snr,cap_PSK);
+desc = strcat('64-QAM');
 hold on;
 xlabel('SNR in dB');
 ylabel('Bits per Symbol');
-legend('QPSK');
+legend(desc);
 grid on;
